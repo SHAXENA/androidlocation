1ef96abf9a77e2f22ce7a484e5d1812d5acf4de9
arormoser <arormoser@4829b6b9-f144-0410-8453-c318896082c8>
2008-02-04 13:03:50 +0000

    Initial import.

---
+++ Android/src/org/apache/maps/Search.java
@@ -0,0 +1,33 @@
+package org.apache.maps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+
+public class Search extends Activity {
+    public Search() {
+    }
+
+    /**
+     * Called with the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        // Set the layout for this activity.  You can find it
+        // in res/layout/hello_activity.xml
+        setContentView(R.layout.main);
+
+        Button addButton = (Button) findViewById(R.id.searchButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                setResult(RESULT_OK, ((EditText)findViewById(R.id.searchText)).getText().toString());
+                finish();
+            } });
+
+    }
+}
